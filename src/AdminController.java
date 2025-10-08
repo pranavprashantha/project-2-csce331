9542435fd0827adaa2b363649932383be36bf364
Owen Thomas <owen123@Computer.localdomain>
2025-10-08 17:32:25 -0500

    added App.java, Controller.java, Controller2.java, main.fxml to src and javafx .jar files in lib. added a gitignore.

---
+++ src/AdminController.java
@@ -0,0 +1,16 @@
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
+import javafx.scene.control.Button;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class AdminController {
+
+}
